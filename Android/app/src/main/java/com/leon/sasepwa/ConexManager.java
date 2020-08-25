f320327dc3d9d1bb921ea57059753b203d00063a
juanseleon1 <juanseleon1@hotmail.com>
2020-08-25 17:58:17 -0500

    Test Conec 1

--- Android/app/src/main/java/com/leon/sasepwa/ConexManager.java
+++ Android/app/src/main/java/com/leon/sasepwa/ConexManager.java
@@ -1,4 +1,54 @@
 package com.leon.sasepwa;
 
-public class ConexManager {
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class  ConexManager {
+    private static final String MY_IP="127.0.0.1";
+    private static final int PORT= 1025;
+    private static InetSocketAddress address=null;
+    private static  ConexManager manager;
+    private ConexManager(){
+        address= new InetSocketAddress(MY_IP,PORT);
+    }
+
+    public static ConexManager getManager(){
+        if(manager==null)
+        {
+            manager= new ConexManager();
+        }
+
+        return manager;
+
+    }
+
+    public void connecTest()
+    {
+        Socket s= new Socket();
+        try {
+            s.connect(address);
+            JSONObject json = new JSONObject();
+            OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream(), StandardCharsets.UTF_8);
+            out.write(json.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
