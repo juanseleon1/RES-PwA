3cc7b8c8d88fe5b1947bae8847f99b5908d5386f
juanseleon1 <juanseleon1@hotmail.com>
2020-09-23 18:33:41 -0500

    Besa Added

--- BESA/AgentePepper/AgentePepper/src/RobotAgentBDI/RobotAgentBDI.java
+++ BESA/AgentePepper/AgentePepper/src/RobotAgentBDI/RobotAgentBDI.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package agentepepper;
+package RobotAgentBDI;
 
 /**
  *
  * @author juans
  */
-public class AgentePepper {
+public class RobotAgentBDI extends AgentBDI{
 
     /**
      * @param args the command line arguments
