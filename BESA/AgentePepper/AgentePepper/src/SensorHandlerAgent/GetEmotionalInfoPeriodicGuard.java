e0af27b2d4087dc4dff2827762f313da34d26be2
juanseleon1 <juanseleon1@hotmail.com>
2020-09-23 19:51:14 -0500

    Besa Agents Initial Guards

---
+++ BESA/AgentePepper/AgentePepper/src/SensorHandlerAgent/GetEmotionalInfoPeriodicGuard.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SensorHandlerAgent;
+
+import BESA.Kernel.Agent.Event.EventBESA;
+import BESA.Kernel.Agent.PeriodicGuardBESA;
+
+/**
+ *
+ * @author juans
+ */
+public class GetEmotionalInfoPeriodicGuard extends PeriodicGuardBESA{
+
+    @Override
+    public void funcPeriodicExecGuard(EventBESA ebesa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
---
+++ BESA/AgentePepper/AgentePepper/src/SensorHandlerAgent/RequestInfoGuard.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SensorHandlerAgent;
+
+import PwAProfileManagerAgent.*;
+import BESA.Kernel.Agent.Event.EventBESA;
+import BESA.Kernel.Agent.GuardBESA;
+
+/**
+ *
+ * @author juans
+ */
+public class RequestInfoGuard extends GuardBESA{
+
+    @Override
+    public void funcExecGuard(EventBESA ebesa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
