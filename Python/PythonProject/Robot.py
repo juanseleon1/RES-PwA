8a4f9caade397951dc3e0c0c695ded0cc0f71599
Brayan Ricardo Garcia Morales <brayangarcia44@hotmail.com>
2021-02-20 12:40:28 -0500

    cambios

--- Python/PythonProject/Robot.py
+++ Python/PythonProject/Robot.py
@@ -122,7 +122,7 @@ class Robot:
             "ACTIVATECONVTOPIC": [self.activate_conversational_topic, True, "act", False],
             "LOADCONVTOPIC": [self.load_conversational_topic, True, "act", False],
             "UNLOADCONVTOPIC": [self.unload_conversational_topic, True, "act", False],
-            "DEACTCONVTOPIC": [self.deactivate_conversational_topic, True, "act", False],
+            "DEACTCONVTOPIC": [self.desactivate_conversational_topic, True, "act", False],
             "SAYUNDERTOPICCONTEXT": [self.say_under_topic_context, True, "act", True],
             "SETTOPICFOCUS": [self.set_topic_focus, True, "act", False]
         }
@@ -789,6 +789,10 @@ class Robot:
         self.alTexToSpeech.setParameter("pitchShift", pitch)
         self.alTexToSpeech.say(text_to_speech)
 
+    def activate_conversational_topic(self):
+        pass
+    def desactivate_conversational_topic(self):
+        pass
     def registrar_cuidador(params):
         pass
 
