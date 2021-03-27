09ba54fa3fd499eb31b99aefefbb7bb2b1910853
juanseleon1 <juanseleon1@hotmail.com>
2021-03-27 17:41:45 -0500

    scripts changed

--- scriptsDocker/run_dockerDB.sh
+++ scriptsDocker/run_dockerDB.sh
@@ -1,1 +1,7 @@
-docker run -d -p 1521:1521 --name oracle-db store/oracle/database-enterprise:12.2.0.1
+CURDIR=$(pwd)
+ORADIR="$CURDIR/oracledb"
+if [ ! -d "$ORADIR" ]; then
+mkdir "$ORADIR"
+chmod 777 "$ORADIR"
+fi
+docker run -d -p 1521:1521 -v "$ORADIR":/u01/app/oracle --name oracle-db store/oracle/database-enterprise:12.2.0.1
