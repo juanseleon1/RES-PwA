44d171dc73ff84d81cf10b69b244696bf9ebc3c6
juanseleon1 <juanseleon1@hotmail.com>
2021-05-13 15:40:23 -0500

    First task fixed

--- BESA/AgenteResPwA/ResPwAAgent/src/Tareas/LogIn/ConversacionInicial.java
+++ BESA/AgenteResPwA/ResPwAAgent/src/Tareas/LogIn/ConversacionInicial.java
@@ -8,10 +8,11 @@ package Tareas.LogIn;
 import RobotAgentBDI.Believes.RobotAgentBelieves;
 import RobotAgentBDI.ResPwaTask;
 import RobotAgentBDI.ServiceRequestDataBuilder.ServiceRequestBuilder;
-import ServiceAgentResPwA.HumanServices.HumanServiceRequestType;
 import ServiceAgentResPwA.ServiceDataRequest;
 import ServiceAgentResPwA.VoiceServices.PepperTopicsNames;
 import ServiceAgentResPwA.VoiceServices.VoiceServiceRequestType;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.HashMap;
 import rational.mapping.Believes;
 
@@ -19,34 +20,41 @@ import rational.mapping.Believes;
  *
  * @author mafegarces
  */
-public class ConversacionInicial extends ResPwaTask{
-    
-    private HashMap<String,Object> infoServicio = new HashMap<>();
-    
+public class ConversacionInicial extends ResPwaTask {
+
+    private HashMap<String, Object> infoServicio = new HashMap<>();
+    private LocalTime start;
+
     public ConversacionInicial() {
 //        System.out.println("--- Task Preguntar Estado Animo Iniciada ---");
+        start = LocalTime.now();
     }
-    
+
     @Override
     public void executeTask(Believes parameters) {
-        System.out.println("--- Execute Task Preguntar Estado Animo ---");
-        RobotAgentBelieves rab= (RobotAgentBelieves)parameters;
-        
-        activateTopic(PepperTopicsNames.SALUDARTOPIC, parameters);
-        infoServicio.put("SAY", "Buen dia, como se encuentra");
-        ServiceDataRequest srb = ServiceRequestBuilder.buildRequest(VoiceServiceRequestType.SAY, infoServicio);
-        requestService(srb,rab);
-        rab.getbEstadoInteraccion().setLogged(true);
-        
+        LocalTime now = LocalTime.now();
+        if (Duration.between(start, now).getSeconds()>30) {
+            System.out.println("--- Execute Task Preguntar Estado Animo ---");
+            RobotAgentBelieves rab = (RobotAgentBelieves) parameters;
+            activateTopic(PepperTopicsNames.SALUDARTOPIC, parameters);
+            if(!infoServicio.containsKey("SAY")){
+                infoServicio.put("SAY", "Buen dia, como se encuentra");
+            }
+            ServiceDataRequest srb = ServiceRequestBuilder.buildRequest(VoiceServiceRequestType.SAY, infoServicio);
+            requestService(srb, rab);
+            start= LocalTime.now();
+        }
+
     }
 
     @Override
     public void interruptTask(Believes believes) {
         System.out.println("--- Interrupt Task Preguntar Estado Animo ---");
         RobotAgentBelieves blvs = (RobotAgentBelieves) believes;
-        if(blvs.getbEstadoInteraccion().isEstaHablando()) {
+        deactivateTopic(PepperTopicsNames.SALUDARTOPIC, believes);
+        if (blvs.getbEstadoInteraccion().isEstaHablando()) {
             ServiceDataRequest srb = ServiceRequestBuilder.buildRequest(VoiceServiceRequestType.STOPALL, null);
-            requestService(srb,blvs);
+            requestService(srb, blvs);
         }
     }
 
@@ -54,22 +62,24 @@ public class ConversacionInicial extends ResPwaTask{
     public void cancelTask(Believes believes) {
         System.out.println("--- Cancel Task Preguntar Estado Animo ---");
         RobotAgentBelieves blvs = (RobotAgentBelieves) believes;
-        if(blvs.getbEstadoInteraccion().isEstaHablando()) {
+        deactivateTopic(PepperTopicsNames.SALUDARTOPIC, believes);
+        if (blvs.getbEstadoInteraccion().isEstaHablando()) {
             ServiceDataRequest srb = ServiceRequestBuilder.buildRequest(VoiceServiceRequestType.STOPALL, null);
-            requestService(srb,blvs);
+            requestService(srb, blvs);
         }
     }
 
     @Override
     public boolean checkFinish(Believes believes) {
-                super.checkFinish(believes);
+        super.checkFinish(believes);
 
         RobotAgentBelieves blvs = (RobotAgentBelieves) believes;
-        if(!blvs.getbEstadoInteraccion().isEstaHablando() && blvs.getbEstadoInteraccion().isRecibirRespuestaPwA()) {
+        if (!blvs.getbEstadoInteraccion().isEstaHablando() && blvs.getbEstadoInteraccion().isRecibirRespuestaPwA()) {
+            deactivateTopic(PepperTopicsNames.SALUDARTOPIC, believes);
+            blvs.getbEstadoInteraccion().setLogged(true);
             return true;
         }
         return false;
     }
-    
-}
 
+}
