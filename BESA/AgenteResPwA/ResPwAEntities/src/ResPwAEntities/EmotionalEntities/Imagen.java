0615479bba53e95e496072fc710f9c85141c801e
juanseleon1 <juanseleon1@hotmail.com>
2021-05-31 17:50:24 -0500

    fin

---
+++ BESA/AgenteResPwA/ResPwAEntities/src/ResPwAEntities/EmotionalEntities/Imagen.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ResPwAEntities.EmotionalEntities;
+
+import ResPwAEntities.*;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author juans
+ */
+@Entity
+@Table(name = "IMAGEN")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Imagen.findAll", query = "SELECT e FROM Imagen e"),
+    @NamedQuery(name = "Imagen.findById", query = "SELECT e FROM Imagen e WHERE e.id = :id")})
+public class Imagen implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private String id;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "url")
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+}
